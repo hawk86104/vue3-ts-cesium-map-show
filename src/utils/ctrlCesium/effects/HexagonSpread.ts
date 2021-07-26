bead3aaa152e190c80595f3587b4d4a1f4b280ca
hawk86104 <hawk86104@163.com>
2021-07-26 19:46:25 +0800

    增加了 六边形扩散的延迟参数

--- src/utils/ctrlCesium/effects/HexagonSpread.ts
+++ src/utils/ctrlCesium/effects/HexagonSpread.ts
@@ -8,23 +8,19 @@ class HexagonSpread {
   constructor(viewer: any) {
     this.viewer = viewer
   }
-  add(position: any, maxRadius: number, color: string) {
+  add(position: any, color: string, maxRadius: number, duration: number) {
     init()
     let e = 0.1
     this.viewer.entities.add({
       position: Cesium.Cartesian3.fromDegrees(position[0], position[1], position[2]),
       ellipse: {
         semiMajorAxis: new Cesium.CallbackProperty(function(n: any) {
-          return (e += 8) > maxRadius && (e = 0.1), e
+          return (e += duration) > maxRadius && (e = 0.1), e
         }, false),
         semiMinorAxis: new Cesium.CallbackProperty(function(n: any) {
           return e
         }, false),
-        material: new Cesium.HexagonSpreadMaterialProperty(
-          {
-            color: new Cesium.Color.fromCssColorString(color),
-            duration: 10000
-          }),
+        material: new Cesium.HexagonSpreadMaterialProperty(new Cesium.Color.fromCssColorString(color)),
       },
     })
   }
