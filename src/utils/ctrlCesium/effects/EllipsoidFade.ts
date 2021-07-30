d4a38e4451a1cbea1aa55474dac7fee80c689afd
hawk86104 <hawk86104@163.com>
2021-07-30 11:07:16 +0800

    持续优化 扩散圈的 entity 方式

--- src/utils/ctrlCesium/effects/EllipsoidFade.ts
+++ src/utils/ctrlCesium/effects/EllipsoidFade.ts
@@ -11,6 +11,8 @@ class EllipsoidFade {
   add(position: any, color: string, maxRadius: number, duration: number) {
     init()
     debugger
+    let currentRadius = 1
+
     this.viewer.entities.add({
       position: Cesium.Cartesian3.fromDegrees(
         position[0],
@@ -22,8 +24,16 @@ class EllipsoidFade {
         // extrudedHeightReference: Cesium.HeightReference.RELATIVE_TO_GROUND,
         // height: position[2],
         // extrudedHeight: position[2], // 如果这里设置高度 那么就会直接穿透 不洒在建筑物上
-        semiMinorAxis: maxRadius || 1000,
-        semiMajorAxis: maxRadius || 1000,
+        semiMajorAxis: new Cesium.CallbackProperty(function(n: any) {
+          currentRadius += (1000 / duration) * 50
+          if (currentRadius > maxRadius) {
+            currentRadius = 1
+          }
+          return currentRadius
+        }, false),
+        semiMinorAxis: new Cesium.CallbackProperty(function(n: any) {
+          return currentRadius
+        }, false),
         material: new Cesium.EllipsoidFadeMaterialProperty(
           new Cesium.Color.fromCssColorString(color),
           duration
