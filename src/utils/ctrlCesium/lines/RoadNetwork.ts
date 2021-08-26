6a4d9158a683a1d94db4b8ab5c2c9c3f410b9977
hawk86104 <hawk86104@163.com>
2021-08-26 15:55:44 +0800

    线效果的demo已经导入，下面是 结构化 可视化编辑

---
+++ src/utils/ctrlCesium/lines/RoadNetwork.ts
@@ -0,0 +1,161 @@
+/*
+ * @Description: 大型路网
+ * @Version: 1.668
+ * @Autor: Hawk
+ * @Date: 2021-08-26 10:48:22
+ * @LastEditors: Hawk
+ * @LastEditTime: 2021-08-26 15:20:18
+ */
+/* eslint-disable no-debugger */
+import { init } from './MaterialProperty/PolylineTrailMaterialProperty'
+import { init as init2 } from './MaterialProperty/Spriteline1MaterialProperty'
+
+import * as turf from '@turf/turf'
+class RoadNetwork {
+  viewer: any
+  id: string
+  constructor(viewer: any, id: string) {
+    this.viewer = viewer
+    this.id = id
+  }
+  // 飞线
+  loadFlyLines() {
+    init()
+    const _this = this
+    Cesium.Resource.fetchJson('./effect/spriteline/nanshan-road1.geojson').then(
+      function(res: any) {
+        let bbox = turf.bbox(res)
+        let points = turf.randomPoint(100, {
+          bbox: bbox,
+        }) // 生成随机点
+        let features = points.features
+        let point: any
+        let startPosition: any
+        let endPosition: any
+        features.forEach((item) => {
+          point = item.geometry.coordinates
+          startPosition = Cesium.Cartesian3.fromDegrees(point[0], point[1], 0)
+          endPosition = Cesium.Cartesian3.fromDegrees(
+            point[0],
+            point[1],
+            3000 * Math.random()
+          )
+          _this.viewer.entities.add({
+            polyline: {
+              positions: [startPosition, endPosition],
+              width: 2,
+              material: new Cesium.PolylineTrailMaterialProperty({
+                speed: 6 * Math.random(),
+                color: new Cesium.Color.fromCssColorString('#32B47E'),
+                percent: 0.1,
+                gradient: 0.01,
+              }),
+            },
+          })
+        })
+      }
+    )
+  }
+  // 道路穿梭线
+  loadRoadPicEffect() {
+    init2()
+    const _this = this
+    let promise = Cesium.GeoJsonDataSource.load(
+      './effect/spriteline/nanshan-road1.geojson'
+    )
+    promise.then(function(dataSource: any) {
+      _this.viewer.dataSources.add(dataSource)
+      const entities = dataSource.entities.values
+      for (let i = 0; i < entities.length; i++) {
+        const entity = entities[i]
+        entity.polyline.width = 3.0
+        entity.polyline.material = new Cesium.Spriteline1MaterialProperty(
+          2000,
+          './effect/spriteline/spriteline1.png'
+        )
+      }
+    })
+
+    promise = Cesium.GeoJsonDataSource.load(
+      './effect/spriteline/nanshan-road2.geojson'
+    )
+    promise.then(function(dataSource: any) {
+      _this.viewer.dataSources.add(dataSource)
+      const entities = dataSource.entities.values
+      for (let i = 0; i < entities.length; i++) {
+        const entity = entities[i]
+        entity.polyline.width = 3.0
+        entity.polyline.material = new Cesium.Spriteline1MaterialProperty(
+          1000,
+          './effect/spriteline/spriteline2.png'
+        )
+      }
+    })
+
+    promise = Cesium.GeoJsonDataSource.load(
+      './effect/spriteline/nanshan-road3.geojson'
+    )
+    promise.then(function(dataSource: any) {
+      _this.viewer.dataSources.add(dataSource)
+      const entities = dataSource.entities.values
+      for (let i = 0; i < entities.length; i++) {
+        const entity = entities[i]
+        entity.polyline.width = 3.0
+        entity.polyline.material = new Cesium.Spriteline1MaterialProperty(
+          1000,
+          './effect/spriteline/spriteline3.png'
+        )
+      }
+    })
+  }
+  loadBeijingLinesData() {
+    const _this = this
+    Cesium.Resource.fetchJson('./effect/bj-lines-bus.json').then(function(
+      i: any
+    ) {
+      let t = []
+      i.map(function(e: any, i: number) {
+        let n = []
+        let s = []
+        for (let a = 0; a < e.length; a += 2) {
+          let r = [e[a], e[a + 1]]
+          a > 0 && (r = [n[0] + r[0], n[1] + r[1]])
+          n = r
+          let o = r[0] / 1e4
+          let c = r[1] / 1e4
+          s.push(o)
+          s.push(c)
+        }
+        t.push({
+          positions: s,
+          color: new Cesium.Color(
+            0,
+            0.8 * Math.random() + 0.2,
+            0.5 * Math.random() + 0.5,
+            1
+          ),
+          width: 2,
+        })
+      })
+      _this.addLineDatas(t)
+    })
+  }
+  addLineDatas(e: any) {
+    const _this = this
+    e.forEach(function(e: any) {
+      _this.viewer.entities.add({
+        polyline: {
+          positions: Cesium.Cartesian3.fromDegreesArray(e.positions),
+          width: e.width,
+          material: new Cesium.PolylineTrailMaterialProperty({
+            speed: 30 * Math.random(),
+            color: e.color,
+            percent: 0.01,
+            gradient: 0.1,
+          }),
+        },
+      })
+    })
+  }
+}
+export default RoadNetwork
