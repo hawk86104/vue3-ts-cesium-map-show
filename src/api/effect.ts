2a236e97dc5c96a014737f2e3023fcdf10efb06d
hawk86104 <hawk86104@163.com>
2021-09-28 16:31:50 +0800

    线效果集合可视化编辑 结束，等待打包

--- src/api/effect.ts
+++ src/api/effect.ts
@@ -4,9 +4,13 @@
  * @Autor: Hawk
  * @Date: 2021-08-23 13:54:51
  * @LastEditors: Hawk
- * @LastEditTime: 2021-08-23 14:10:30
+ * @LastEditTime: 2021-09-28 15:58:20
  */
 import { http } from '../utils/http'
 export const getPointsEffectList = (): any => {
   return http.request('get', '/effect/getPointsEffectList')
+}
+
+export const getLinesEffectList = (): any => {
+  return http.request('get', '/effect/getLinesEffectList')
 }
